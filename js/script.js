1e1c764978869a9941afd3cffbb8e204eb4ceb85
Unknown <stmihailov@yandex.ru>
2018-06-17 20:39:57 +0300

    правки

---
+++ js/script.js
@@ -0,0 +1,17 @@
+'use strict'
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
